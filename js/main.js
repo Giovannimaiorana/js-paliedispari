cab6831eec8437de7ed602aab5f590a3057b34da
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-03 15:48:47 +0100

    esercizio parola palindroma finito

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//CHIEDERE ALL UTENTE SE SCELIERE TRA PARI O DISPARI
+/*/CHIEDERE ALL UTENTE SE SCELIERE TRA PARI O DISPARI
 let pariDispari = prompt("scegli pari o dispari");
 console.log(pariDispari);
 
@@ -33,4 +33,37 @@ if (risultato == true){
     console.log("numero pari")
 } else{
     console.log("numero dispari")
+};
+*/
+
+
+//ESERCIZIO PAROLA PALINDROMA
+
+
+// CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA
+
+let parolaUtente = prompt("Inserisci una parola");
+console.log(parolaUtente);
+
+//CREIAMO UNA FUNZIONE PER CAPIRE SE LA PARTOLA è PALINDROMA 
+
+
+function parolaPalindroma(parola){
+ let i = 0
+ let x = parolaUtente.length - 1; 
+ while (i < x){
+    if(parolaUtente[i] !== parola[x]){
+        return false
+    }
+    i++;
+    x--;
+ }
+    return true;
 }
+
+//VERIFICHIAMO SE LA PAROLA è PALINDROMA
+if (parolaPalindroma(parolaUtente)){
+    console.log(parolaUtente + " è palindorma");
+} else{
+    console.log(parolaUtente + " non è palindroma");
+};
